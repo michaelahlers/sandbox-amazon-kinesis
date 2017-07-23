8d1bd59f206b9eaec0da0f1deb02f5b064fb1b6e
Michael Ahlers <michael@ahlers.co>
2017-07-23 11:39:39 -0500

    Generate random trades

---
+++ src/main/scala/ahlers/sandbox/amazon/kinesis/stocks/StockTrades.scala
@@ -0,0 +1,31 @@
+package ahlers.sandbox.amazon.kinesis.stocks
+
+import scala.util.Random
+
+/**
+ * @see [[https://github.com/awslabs/amazon-kinesis-learning/blob/181ee5830b4401b54e64c9a784ea2973d96e381a/src/com/amazonaws/services/kinesis/samples/stocktrades/writer/StockTradeGenerator.java StockTradeGenerator]]
+ */
+object StockTrades {
+
+  val MaxDeviation = 0.2
+
+  val MaxQuantity = 10000
+
+  val ProbabilitySell = 0.4
+
+  def random: StockTrade = {
+    import StockTrade._
+    import TradeType._
+
+    val stockPrice = StockPrices.random
+    import stockPrice._
+
+    val deviation = (Random.nextDouble() - 0.5) * 2.0 * MaxDeviation
+    val price = stockPrice.price * (1 + deviation)
+    val tradeType = if (Random.nextDouble() < ProbabilitySell) Sell else Buy
+    val quantity = Random.nextInt(MaxQuantity) + 1
+
+    StockTrade(tickerSymbol, tradeType, price, quantity)
+  }
+
+}
